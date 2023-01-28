e28a2bf2a317045230d2109f67a4f4f4b0e1cbe9
Rahul Raj <rahulfrndz02@gmail.com>
2023-01-28 21:47:35 +0530

    changes

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -24,56 +24,60 @@ public class ReservationServiceImpl implements ReservationService {
     ParkingLotRepository parkingLotRepository3;
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+        try{
+            ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
 
-        ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+            User user = userRepository3.findById(userId).get();
 
-        User user = userRepository3.findById(userId).get();
-
-        if(parkingLot==null || user==null){
-            throw new Exception("Cannot make reservation");
-        }
-
-        List<Spot> spotList = parkingLot.getSpotList();
-
-        Integer minbill = Integer.MAX_VALUE;
-
-        Spot reqspot=null;
-        for(Spot spot:spotList){
-            if(returnSpot(spot.getSpotType())>=numberOfWheels && spot.getOccupied()==false && spot.getPricePerHour()*timeInHours<minbill){
-                minbill=spot.getPricePerHour()*timeInHours;
-                reqspot=spot;
+            if(parkingLot==null || user==null){
+                throw new Exception("Cannot make reservation");
             }
+
+            List<Spot> spotList = parkingLot.getSpotList();
+
+            Integer minBill = Integer.MAX_VALUE;
+
+            Spot reqspot=null;
+            for(Spot spot:spotList){
+                if(spotTypeFunction(spot.getSpotType())>=numberOfWheels && spot.getOccupied()==false && spot.getPricePerHour()*timeInHours<minBill){
+                    minBill=spot.getPricePerHour()*timeInHours;
+                    reqspot=spot;
+                }
+            }
+            if (reqspot==null){
+                throw new Exception("Cannot make reservation");
+            }
+
+            Reservation reservation = new Reservation(timeInHours);
+            reservation.setUser(user);
+            List<Reservation> reservationList = user.getReservationList();
+            if(reservationList==null){
+                reservationList = new ArrayList<>();
+            }
+            reservationList.add(reservation);
+            user.setReservationList(reservationList);
+
+
+            reservation.setSpot(reqspot);
+            List<Reservation> reservationList1 = reqspot.getReservationList();
+            if(reservationList1==null){
+                reservationList1 = new ArrayList<>();
+            }
+            reservationList1.add(reservation);
+            reqspot.setReservationList(reservationList1);
+            reqspot.setOccupied(true);
+
+            userRepository3.save(user);
+            spotRepository3.save(reqspot);
+
+            return reservation;
+
+        }catch (Exception e){
+            return null;
         }
-        if (reqspot==null){
-            throw new Exception("Cannot make reservation");
-        }
-
-        Reservation reservation = new Reservation(timeInHours);
-        reservation.setUser(user);
-        List<Reservation> reservationList = user.getReservationList();
-        if(reservationList==null){
-            reservationList = new ArrayList<>();
-        }
-        reservationList.add(reservation);
-        user.setReservationList(reservationList);
-
-
-        reservation.setSpot(reqspot);
-        List<Reservation> reservationList1 = reqspot.getReservationList();
-        if(reservationList1==null){
-            reservationList1 = new ArrayList<>();
-        }
-        reservationList1.add(reservation);
-        reqspot.setReservationList(reservationList1);
-        reqspot.setOccupied(true);
-
-        userRepository3.save(user);
-        spotRepository3.save(reqspot);
-
-        return reservation;
     }
 
-    public int returnSpot(SpotType spotType){
+    public int spotTypeFunction(SpotType spotType){
         if (spotType==SpotType.TWO_WHEELER){
             return 2;
         }
